83bd4e4c6c463b9dd8d38ab7a2aabf41b9f8d4fe
Abdi Berhe <abdi201380@gmail.com>
2023-05-23 15:43:45 -0700

    [File]: js code after fix

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -19,6 +19,7 @@ const size = parseInt(process.argv[2]);
 if (isNaN(size)) {
 	process.stderr.write("Invalid size\n");
 	process.exit(1);
+}
 
 for (let i = 0 ; i < size ; i ++) {
     for (let j = 0 ; j < size ; j ++) {
