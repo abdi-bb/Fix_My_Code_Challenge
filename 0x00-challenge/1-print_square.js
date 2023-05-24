91f96b46d05ec240dcf63d62abff06d943e89306
Abdi Berhe <abdi201380@gmail.com>
2023-05-24 11:15:47 -0700

    [Update]: post checker

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 /*
     Print a square with the character #
-    
-    The size of the square must be the first argument 
+
+    The size of the square must be the first argument
     of the program.
 */
-
 
 if (process.argv.length <= 2) {
     process.stderr.write("Missing argument\n");
@@ -16,13 +15,13 @@ if (process.argv.length <= 2) {
 
 const size = parseInt(process.argv[2]);
 
-if (isNaN(size)) {
-	process.stderr.write("Invalid size\n");
-	process.exit(1);
+if (isNaN(size) || size <= 0) {
+    process.stderr.write("Invalid size\n");
+    process.exit(1);
 }
 
-for (let i = 0 ; i < size ; i ++) {
-    for (let j = 0 ; j < size ; j ++) {
+for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
         process.stdout.write("#");
     }
     process.stdout.write("\n");
